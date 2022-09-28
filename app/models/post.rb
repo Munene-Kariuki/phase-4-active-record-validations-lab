9ee12d7c549fb97b45b71b632cdba6780487c74d
Denis <denismunene999@gmail.com>
2022-09-28 14:03:47 +0300

    commit tree 1bdabdf285c2611d98c1cefb230b523069b7628f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validate :clickbait?
+
+  TITLE_NOT_CONTAIN = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def clickbait?
+    if TITLE_NOT_CONTAIN.none? { |word| word.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
